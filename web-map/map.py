2a9683cb4afe6679ee805ac6bfd3059cb06e3194
Yanko Atanasov Aleksandrov <superactro@gmail.com>
2021-12-05 14:38:05 +0200

    map2

--- web-map/map.py
+++ web-map/map.py
@@ -4,12 +4,13 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 fg = folium.FeatureGroup(name="My Map")
 
 map = folium.Map(location=[42.1354, 24.7453], zoom_start=6, tiles="Stamen Terrain")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], zoom_start=6, tiles="Stamen Terrain"))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=el, zoom_start=6, tiles="Stamen Terrain"))
 
 map.add_child(fg)
 map.save("Map1.html")
