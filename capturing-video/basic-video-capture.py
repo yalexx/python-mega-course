dbc49295394b7389771f5dc1d781d869075e2267
Yanko Atanasov Aleksandrov <superactro@gmail.com>
2021-12-06 19:31:25 +0200

    video face recognition

---
+++ capturing-video/basic-video-capture.py
@@ -0,0 +1,25 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+video = cv2.VideoCapture(1)
+
+while True:
+    check, frame = video.read()
+
+    print(check)
+    print(frame)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.30, minNeighbors=5)
+    for x, y, w, h in faces:
+        img = cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow("Capturing", gray)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
+
+cv2.destroyAllWindows()
