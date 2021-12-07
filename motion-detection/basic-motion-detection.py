a49229814e247cab9d7304fe29733206d22dc7d0
Yanko Atanasov Aleksandrov <superactro@gmail.com>
2021-12-07 15:27:40 +0200

    motion track

--- motion-detection/basic-motion-detection.py
+++ motion-detection/basic-motion-detection.py
@@ -1,12 +1,14 @@
 import cv2
-from zmq.backend import first
+import datetime
 
 first_frame = None
+status_list = [None, None]
+times = []
 video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -22,21 +24,25 @@ while True:
         thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        status = 1
 
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.datetime.now())
     cv2.imshow("Capturing", gray)
     cv2.imshow("Show", delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
     cv2.imshow("Color frame", frame)
     key = cv2.waitKey(1)
-    print(gray)
-    print(delta_frame)
 
     if key == ord('q'):
         break
+
+    print(status_list)
 
 video.release()
 cv2.destroyAllWindows()
