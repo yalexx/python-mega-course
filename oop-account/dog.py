66ce5b8895b9d71790d53db91c5e0d8304c46866
Yanko Atanasov Aleksandrov <superactro@gmail.com>
2021-12-30 10:14:22 +0200

    flask app

---
+++ oop-account/dog.py
@@ -0,0 +1,35 @@
+class hell0o
+    """This class provides kur"""
+
+    class Dog(Animal):
+
+    def eat(self):
+        print('eating')
+
+
+class Dog(Animal):
+
+
+class Dog(Animal):
+
+
+    class Dog(Animal):
+
+
+    class Dog(Animal):
+
+
+    class Dog(Animal):
+
+
+    class Dog(Animal):
+        def __init__(self):
+            Animal.__init__(self)
+
+        def bark(self):
+            print('bau bau!')
+
+
+zobcho = Dog()
+zobcho.eat()
+zobcho.bark()
